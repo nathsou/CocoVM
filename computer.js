ef7b8f4bb9201540963c6b7190b2b10e9f8c614b
Nathan Soufflet <nathsou.fr@gmail.com>
2016-11-05 18:01:46 +0100

    Created a EventEmitter interface

--- computer.js
+++ computer.js
@@ -11,14 +11,18 @@ let Utils = {
             b.push(bit === '1');
         return b;
     },
-    fillZeros: (num, count) => {
+    fillZeros: (num, count, backwards = false) => {
         let b = [];
         if (typeof (num) === 'number')
             b = Utils.num2bits(num);
         else
             b = num;
-        for (let i = b.length; i < count; i++)
-            b.unshift(false);
+        for (let i = b.length; i < count; i++) {
+            if (!backwards)
+                b.unshift(false);
+            else
+                b.push(false);
+        }
         return b;
     },
     bits2str: (data) => {
@@ -49,8 +53,48 @@ let Utils = {
         };
     },
 };
-class Computer {
+class EventEmitter {
+    constructor() {
+        this.eventHandlers = new Map();
+    }
+    on(ev, handler) {
+        this.eventHandlers.set(ev, handler);
+    }
+    emit(ev, value) {
+        if (this.eventHandlers.has(ev))
+            this.eventHandlers.get(ev).call(this, value);
+    }
+    bindEvent(em, ev) {
+        em.on(ev, (value) => this.emit(ev, value));
+    }
+}
+class RAM extends EventEmitter {
+    constructor(byteLength, nb_bytes) {
+        super();
+        this.byteLength = byteLength;
+        this.capacity = nb_bytes;
+        this.memory = [];
+        for (let i = 0; i < nb_bytes; i++)
+            this.memory[i] = [];
+    }
+    read(addr) {
+        if (addr >= this.capacity)
+            this.emit('error', `Incorrect RAM address: ${addr}`);
+        return this.memory[addr];
+    }
+    write(addr, value) {
+        if (addr >= this.capacity)
+            this.emit('error', `Incorrect RAM address: ${addr}`);
+        if (value.length > this.byteLength)
+            this.emit('error', `Cannot store ${value.length} bits of data in RAM, since 1 byte = ${this.byteLength}`);
+        else if (value.length < this.byteLength)
+            value = Utils.fillZeros(value, this.byteLength);
+        this.memory[addr] = value;
+    }
+}
+class Computer extends EventEmitter {
     constructor(arch) {
+        super();
         this.STEP_LIMIT = 10000;
         this.running = false;
         this.instNames = [
@@ -75,50 +119,51 @@ class Computer {
         this.registers = [];
         for (let i = 0; i < arch.registerCount; i++)
             this.registers[i] = [];
+        this.RAM = new RAM(arch.bits, arch.RAM_bytes);
+        this.bindEvent(this.RAM, 'error');
         this.instructions = [];
         this.status_reg = [false, false]; //overflow, zero
-        this.instAddr = 0;
-        this.eventHandlers = new Map();
+        this.PC = 0;
         this.instCodeBitCount = Math.floor(Math.log(this.instNames.length) / Math.LN2) + 1;
     }
     compile(prog) {
         let binary = [];
+        let bitsPerInst = Math.ceil((this.instCodeBitCount + 2 + this.arch.bits) / this.arch.bits) * this.arch.bits;
         for (let inst of prog.split('\n')) {
-            if (inst.trim() === '')
+            //Translate the instruction into binary
+            //Find current instruction's code
+            inst = inst.trim();
+            let instBinary = [];
+            if (inst === '')
                 continue;
-            let l = inst.trim().split(' ');
-            let n = null;
-            for (let i = 0; i < this.instNames.length; i++) {
-                if (this.instNames[i] === l[0])
-                    n = i;
-            }
-            if (n === null) {
+            let l = inst.split(' ');
+            let n = this.instNames.indexOf(l[0]);
+            if (n === -1)
                 this.emit('error', 'Unknown instruction: ' + l[0]);
-            }
-            for (let bit of Utils.fillZeros(n, this.instCodeBitCount))
-                binary.push(bit);
-            if (l[1] === undefined) {
-                for (let i = 0; i < this.arch.bits; i++)
-                    binary.push(false);
-                continue;
-            }
-            //convert the argument to hex
+            instBinary.push(...Utils.fillZeros(n, this.instCodeBitCount)); //Add instruction code
+            //Handle the argument
+            if (l[1] === undefined)
+                l[1] = '';
             let arg = 0;
+            instBinary.push(l[1][0] === '#'); //numerical value or RAM addr ?
+            instBinary.push(l[1][0] === '%'); //register addr
+            l[1] = l[1].replace('#', '').replace('%', '');
             if (l[1].substr(0, 1) === '$') {
                 arg = parseInt(l[1].replace('$', ''), 16);
             }
             else
                 arg = parseInt(l[1]);
-            for (let bit of Utils.fillZeros(arg, this.arch.bits))
-                binary.push(bit);
+            instBinary.push(...Utils.fillZeros(arg, bitsPerInst - instBinary.length));
+            //binary.push(...Utils.fillZeros(instBinary, bitsPerInst, true));
+            binary.push(...instBinary);
         }
         return binary;
     }
     loadProgram(prog) {
         if (!(prog instanceof String)) {
             this.instructions = [];
-            this.instAddr = 0;
-            let instLen = this.instCodeBitCount + this.arch.bits;
+            this.PC = 0;
+            let instLen = Math.ceil((this.instCodeBitCount + 2 + this.arch.bits) / this.arch.bits) * this.arch.bits;
             for (let i = 0; i < prog.length; i += instLen)
                 this.instructions.push(prog.slice(i, i + instLen));
         }
@@ -126,66 +171,74 @@ class Computer {
             this.loadProgram(Utils.str2bits(prog));
     }
     step() {
-        if (this.instAddr >= this.instructions.length) {
-            this.emit('error', 'No instruction at addr: ' + this.instAddr.toString(16));
+        if (this.PC >= this.instructions.length) {
+            this.emit('error', 'No instruction at addr: ' + this.PC.toString(16));
         }
-        let inst = this.instructions[this.instAddr];
+        let inst = this.instructions[this.PC];
         let icode = Utils.bits2num(inst.slice(0, this.instCodeBitCount));
-        let argBin = inst.slice(this.instCodeBitCount, inst.length);
+        let argBin = inst.slice(this.instCodeBitCount + 2, inst.length);
         let arg = Utils.bits2num(argBin);
+        if (!inst[this.instCodeBitCount]) {
+            argBin = this.RAM.read(arg);
+            arg = Utils.bits2num(argBin);
+        }
+        else if (inst[this.instCodeBitCount + 1]) {
+            argBin = this.getRegister(arg);
+            arg = Utils.bits2num(argBin);
+        }
         switch (icode) {
             case 0:
                 this.setRegister(0, argBin);
-                this.instAddr++;
+                this.PC++;
                 break;
             case 1:
-                this.setRegister(0, this.getRegister(arg));
-                this.instAddr++;
+                this.setRegister(0, argBin);
+                this.PC++;
                 break;
             case 2:
                 this.setRegister(arg, this.getRegister(0));
-                this.instAddr++;
+                this.PC++;
                 break;
             case 3:
-                this.instAddr = arg;
+                this.PC = arg;
                 break;
             case 4:
-                this.setRegister(0, this.ALU(this.getRegister(0), this.getRegister(arg)));
-                this.instAddr++;
+                this.setRegister(0, this.ALU(this.getRegister(0), argBin));
+                this.PC++;
                 break;
             case 5:
                 if (this.status_reg[0])
-                    this.instAddr = arg;
+                    this.PC = arg;
                 else
-                    this.instAddr++;
+                    this.PC++;
                 break;
             case 6:
                 this.emit('OUT', `${Utils.bits2str(this.getRegister(0))} - ${Utils.bits2num(this.getRegister(0))}`);
-                this.instAddr++;
+                this.PC++;
                 break;
             case 7:
                 //TODO
                 break;
             case 8:
                 this.registers[arg] = this.ALU(this.registers[arg], [true]);
-                this.instAddr++;
+                this.PC++;
                 break;
             case 9:
                 this.status_reg[1] = Utils.bits2num(this.getRegister(0)) === arg;
-                this.instAddr++;
+                this.PC++;
                 break;
             case 10:
                 if (!this.status_reg[1]) {
-                    this.instAddr = arg;
+                    this.PC = arg;
                 }
                 else
-                    this.instAddr++;
+                    this.PC++;
                 break;
             case 11:
                 this.running = false;
                 break;
         }
-        this.emit('step', this.instAddr);
+        this.emit('step', this.PC);
     }
     run(clean = true) {
         this.running = true;
@@ -217,14 +270,14 @@ class Computer {
         if (cleanInstructions)
             this.instructions = [];
         this.status_reg = [false, false];
-        this.instAddr = 0;
+        this.PC = 0;
         this.running = false;
         this.emit('reset');
     }
     halt(msg = '') {
         console.warn('Computer halted: ' + msg);
         this.running = false;
-        this.emit('halt');
+        this.emit('halt', msg);
     }
     ALU(a, b, sub = false) {
         let sum = [], carry = false;
@@ -247,12 +300,5 @@ class Computer {
                 this.status_reg[0] = true;
         }
         return sum;
-    }
-    on(ev, handler) {
-        this.eventHandlers.set(ev, handler);
-    }
-    emit(ev, value) {
-        if (this.eventHandlers.has(ev))
-            this.eventHandlers.get(ev).call(this, value);
     }
 }
--- computer.ts
+++ computer.ts
@@ -20,7 +20,7 @@ let Utils = {
         return b;
     },
 
-    fillZeros: (num: number | bits, count: number) : bits => {
+    fillZeros: (num: number | bits, count: number, backwards = false) : bits => {
 
         let b: bits = [];
 
@@ -28,8 +28,10 @@ let Utils = {
             b = Utils.num2bits(num);
         else b = num;
 
-        for (let i = b.length; i < count; i++)
-            b.unshift(false);
+        for (let i = b.length; i < count; i++) {
+            if (!backwards) b.unshift(false);
+            else b.push(false);
+        }
 
         return b;
     },
@@ -77,28 +79,96 @@ let Utils = {
 
 interface Architecture {
     bits: number;
-    registerCount: number
+    registerCount: number,
+    RAM_bytes: number
 }
 
-class Computer {
+abstract class EventEmitter {
+
+    eventHandlers: Map<string, (value) => any>;
+
+    constructor() {
+        this.eventHandlers = new Map<string, (value) => any>();
+    }
+
+    on(ev: string, handler: (value) => any) {
+        this.eventHandlers.set(ev, handler);
+    }
+
+    protected emit(ev: string, value?: any) {
+
+        if (this.eventHandlers.has(ev))
+            this.eventHandlers.get(ev).call(this, value);
+    }
+
+    protected bindEvent(em: EventEmitter, ev: string) {
+
+        em.on(ev, (value) => this.emit(ev, value));
+    }
+}
+
+class RAM extends EventEmitter {
+
+    byteLength: number;
+    capacity: number;
+    memory: bits[];
+
+    constructor(byteLength: number, nb_bytes) {
+
+        super();
+
+        this.byteLength = byteLength;
+        this.capacity = nb_bytes;
+
+        this.memory = [];
+
+        for (let i = 0; i < nb_bytes; i++)
+            this.memory[i] = [];
+
+    }
+
+    read(addr: number) : bits {
+
+        if (addr >= this.capacity)
+            this.emit('error', `Incorrect RAM address: ${addr}`);
+
+        return this.memory[addr];
+    }
+
+    write(addr: number, value: bits) : void {
+
+        if (addr >= this.capacity)
+            this.emit('error', `Incorrect RAM address: ${addr}`);
+
+        if (value.length > this.byteLength)
+            this.emit('error', `Cannot store ${value.length} bits of data in RAM, since 1 byte = ${this.byteLength}`);
+        else if (value.length < this.byteLength)
+            value = Utils.fillZeros(value, this.byteLength);
+
+        this.memory[addr] = value;
+    }
+
+}
+
+class Computer extends EventEmitter {
 
     STEP_LIMIT = 10000;
     arch: Architecture;
-    instAddr: number;
+    PC: number; //Program Counter -> addr of the current instruction
     registers: bits[];
+    RAM: RAM;
     instructions: bits[];
     status_reg: bits;
     instCodeBitCount: number;
     running: boolean = false;
-    eventHandlers: Map<string, (value) => any>;
 
     instNames = [
         'AEQ', //AEQ v: v -> A
         'LDA', //LDA n : Load register n to A
         'STA', //STA n : Store A to register n
-        'JMP', //JMP addr : Set instAddr to addr
+        'JMP', //JMP addr : Set PC to addr
         'ADD', //ADD n: value of n + value of A -> A
-        'JC', //JC addr : Set instAddr to addr if overflow is true
+        'JC', //JC addr : Set PC to addr if overflow is true
         'OUT', //OUT: Output value of A
         'SUB', //SUB n: value of n - value of A -> A
         'INC', //INC n: value of n = value of n + 1
@@ -108,6 +178,9 @@ class Computer {
     ];
 
     constructor(arch: Architecture) {
+
+        super();
+
         this.arch = arch;
 
         if (this.arch.registerCount < 1) 
@@ -121,10 +194,12 @@ class Computer {
         for (let i = 0; i < arch.registerCount; i++)
             this.registers[i] = [];
 
+        this.RAM = new RAM(arch.bits, arch.RAM_bytes);
+        this.bindEvent(this.RAM, 'error');
+
         this.instructions = [];
         this.status_reg = [false, false]; //overflow, zero
-        this.instAddr = 0;
-        this.eventHandlers = new Map<string, (value) => any>();
+        this.PC = 0;
 
         this.instCodeBitCount = Math.floor(Math.log(this.instNames.length) / Math.LN2) + 1;
     }
@@ -133,46 +208,55 @@ class Computer {
 
         let binary: bits = [];
 
+        let bitsPerInst = Math.ceil((this.instCodeBitCount + 2 + this.arch.bits) / this.arch.bits) * this.arch.bits;
+
         
         for (let inst of prog.split('\n')) {
 
-            if (inst.trim() === '') continue;
+            //Translate the instruction into binary
 
-            let l = inst.trim().split(' ');
 
-            let n = null;
+            //Find current instruction's code
 
-            for (let i = 0; i < this.instNames.length; i++) {
-                if (this.instNames[i] === l[0])
-                    n = i; 
-            }
+            inst = inst.trim();
 
-            if (n === null) {
+            let instBinary = [];
+
+            if (inst === '') continue;
+
+            let l = inst.split(' ');
+
+            let n = this.instNames.indexOf(l[0]);
+
+            if (n === -1) 
                 this.emit('error', 'Unknown instruction: ' + l[0]);
-            }
 
-            
-            for (let bit of Utils.fillZeros(n, this.instCodeBitCount)) //Add instruction code
-                binary.push(bit);
+            instBinary.push(...Utils.fillZeros(n, this.instCodeBitCount)); //Add instruction code
 
-            if (l[1] === undefined) {
-                for (let i = 0; i < this.arch.bits; i++)
-                    binary.push(false);
 
-                continue;
-            }
+            //Handle the argument
 
-            //convert the argument to hex
+            if (l[1] === undefined) l[1] = '';
 
             let arg = 0; 
+
+            instBinary.push(l[1][0] === '#'); //numerical value or RAM addr ?
+            instBinary.push(l[1][0] === '%'); //register addr
+
+            l[1] = l[1].replace('#', '').replace('%', '');
 
             if (l[1].substr(0, 1) === '$') { //hex
                 arg = parseInt(l[1].replace('$', ''), 16);
             } else //decimal
                 arg = parseInt(l[1]);
 
-            for (let bit of Utils.fillZeros(arg, this.arch.bits))
-                binary.push(bit);
+
+            instBinary.push(...Utils.fillZeros(arg, bitsPerInst - instBinary.length));
+
+            
+            //binary.push(...Utils.fillZeros(instBinary, bitsPerInst, true));
+
+            binary.push(...instBinary);
 
         }
 
@@ -186,9 +270,9 @@ class Computer {
 
         this.instructions = [];
 
-        this.instAddr = 0;
+        this.PC = 0;
 
-        let instLen = this.instCodeBitCount + this.arch.bits;
+        let instLen =  Math.ceil((this.instCodeBitCount + 2 + this.arch.bits) / this.arch.bits) * this.arch.bits;
 
         for (let i = 0; i < prog.length; i += instLen)
             this.instructions.push(prog.slice(i, i + instLen));
@@ -200,31 +284,42 @@ class Computer {
 
     step() : void { //runs one clock cycle
 
-        if (this.instAddr >= this.instructions.length) {
-            this.emit('error', 'No instruction at addr: ' + this.instAddr.toString(16))
+        if (this.PC >= this.instructions.length) {
+            this.emit('error', 'No instruction at addr: ' + this.PC.toString(16))
         }
 
-        let inst = this.instructions[this.instAddr];
+        let inst = this.instructions[this.PC];
 
         let icode = Utils.bits2num(inst.slice(0, this.instCodeBitCount));
-        let argBin = inst.slice(this.instCodeBitCount, inst.length);
+        
+        let argBin = inst.slice(this.instCodeBitCount + 2, inst.length);
         let arg = Utils.bits2num(argBin);
+
+        if (!inst[this.instCodeBitCount]) { //arg is a RAM address
+            argBin = this.RAM.read(arg);
+            arg = Utils.bits2num(argBin);
+        } else if (inst[this.instCodeBitCount + 1]) { //arg is a register address
+            argBin = this.getRegister(arg);
+            arg = Utils.bits2num(argBin);  
+        }
+
+
 
         switch (icode) {
             case 0: //AEQ
 
                 this.setRegister(0, argBin);
 
-                this.instAddr++;
+                this.PC++;
 
                 break;
 
             case 1: //LDA
 
 
-                this.setRegister(0, this.getRegister(arg));
+                this.setRegister(0, argBin);
 
-                this.instAddr++;
+                this.PC++;
 
                 break;
 
@@ -232,29 +327,29 @@ class Computer {
 
                 this.setRegister(arg, this.getRegister(0));
 
-                this.instAddr++;
+                this.PC++;
 
                 break;
 
             case 3: //JMP
 
-                this.instAddr = arg;
+                this.PC = arg;
 
                 break;
 
             case 4: //ADD
 
-                this.setRegister(0, this.ALU(this.getRegister(0), this.getRegister(arg)));
+                this.setRegister(0, this.ALU(this.getRegister(0), argBin));
 
-                this.instAddr++;
+                this.PC++;
 
                 break;
 
             case 5: //JC
 
                 if (this.status_reg[0])
-                    this.instAddr = arg;
-                else this.instAddr++;
+                    this.PC = arg;
+                else this.PC++;
 
                 break;
 
@@ -262,7 +357,7 @@ class Computer {
 
                 this.emit('OUT', `${Utils.bits2str(this.getRegister(0))} - ${Utils.bits2num(this.getRegister(0))}`);
 
-                this.instAddr++;
+                this.PC++;
 
                 break;
 
@@ -276,7 +371,7 @@ class Computer {
 
                 this.registers[arg] = this.ALU(this.registers[arg], [true]);
 
-                this.instAddr++;
+                this.PC++;
 
                 break;
 
@@ -284,15 +379,15 @@ class Computer {
 
                 this.status_reg[1] = Utils.bits2num(this.getRegister(0)) === arg;
 
-                this.instAddr++;
+                this.PC++;
 
                 break;
 
             case 10: //JNEQ
 
                 if (!this.status_reg[1]) {
-                    this.instAddr = arg;
-                } else this.instAddr++;
+                    this.PC = arg;
+                } else this.PC++;
 
                 break;
 
@@ -303,7 +398,7 @@ class Computer {
                 break;
         }
 
-        this.emit('step', this.instAddr);
+        this.emit('step', this.PC);
     }
 
     run(clean: boolean = true) : void { //runs instructions until HLT
@@ -349,7 +444,7 @@ class Computer {
         if (cleanInstructions) this.instructions = [];
 
         this.status_reg = [false, false];
-        this.instAddr = 0;
+        this.PC = 0;
         this.running = false;
 
         this.emit('reset');
@@ -359,7 +454,7 @@ class Computer {
         console.warn('Computer halted: ' + msg);
         this.running = false;
 
-        this.emit('halt');
+        this.emit('halt', msg);
     }
 
     ALU(a: bits, b: bits, sub = false) : bits {
@@ -390,18 +485,5 @@ class Computer {
         return sum;
 
     }
-
-    on(ev: string, handler: (value) => any) {
-
-        this.eventHandlers.set(ev, handler);
-
-    } 
-
-    protected emit(ev: string, value?: any) {
-
-        if (this.eventHandlers.has(ev))
-            this.eventHandlers.get(ev).call(this, value);
-    }
-    
 
 }
